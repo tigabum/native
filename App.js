713c8bd726490983c1c00067b8869beae47003b4
Tigabu <tigabutg@gmai.com>
2021-04-20 01:15:08 +0300

    native

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './Screens/Login'
+import Home from './Screens/Home';
+
+const Stack = createStackNavigator();
+const screenOptionsContainer = {
+headerStyle:{
+  backgroundColor:'#00780D'
+},
+headerTitleStyle:{
+  color:'white'
+}
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator screenOptions={screenOptionsContainer} >
+       <Stack.Screen name="Login" component={Login} options={{title:'Log In'}} />
+       
+     </Stack.Navigator>
+
+    </NavigationContainer>
+    
   );
 }
 
---
+++ Screens/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const Login = () => {
+    return (
+        <View >
+            <Text>This is Login Page</Text>
+        </View>
+    )
+}
+
+export default Login
+const styles = StyleSheet.create({
+   
+})
